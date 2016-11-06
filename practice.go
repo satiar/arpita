fe8abc48847e27120b9d31e298d5408e10071cee
Arpita Sati <arpita.sati@hpe.com>
2016-11-06 10:56:21 -0800

    Create practice.go

---
+++ practice.go
@@ -0,0 +1,23 @@
+SELECTION SORT:
+func selectionSort(arr []int) []int{
+	for i := 0; i < len(arr) ; i++ {
+		min := i
+		fmt.Println(min)
+		for j := i + 1; j < len(arr) ; j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		arr = swap(arr, i, min)
+	}
+	fmt.Println(arr)
+	return arr
+}
+
+func swap(arr []int, i, j int) []int{
+	fmt.Println(i, j)
+	tmp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = tmp
+	return arr
+}
