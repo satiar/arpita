a3a431fc5d2a8aec9df8d5cf2999a89e3c53b922
Arpita Sati <arpita.sati@hpe.com>
2016-11-07 15:33:35 -0800

    Update strings.go

--- InterviewPrep/strings.go
+++ InterviewPrep/strings.go
@@ -150,9 +150,56 @@ func findCount(s string, char string) int {
 	return strings.Count(strings.ToLower(s), char)
 }
 
-10. Replace all occurences of whitespace with %20func replaceWhitespace(str string) string {
+10. Replace all occurences of whitespace with %20
+func replaceWhitespace(str string) string {
 	str = strings.Trim(str," ")
 	//replace (string, old, new , n(if <0, then replace all occurences))
 	//if n =2, replae first 2 occurences etc...
 	return strings.Replace(str," ", "%20", 2)	
 }
+
+11. Find all permutations of a characters of a string (n!)
+Algo: Example, 
+Find all perm of abc:
+We will find all perms of the given string based on all perms of its substring
+To find all perms of abc, we need to find all perms of bc, and then add a to those perms
+To find all perms of bc, we need to find all perms of c, and add b to those perms
+To find all perms of c, well we know that is only c
+
+Now we can find all perms of bc, insert c at every available space in b
+bc --> bc, cb
+Now we need to add a to all perms of bc
+abc, bac, bca (insert a in bc) -- acb, cab, cba (insert a in cb)
+
+func getPermutations(str string) []string {
+	// base case, for one char, all perms are [char]
+	if len(str) == 1 {
+		return []string{str}
+	}
+
+	current := str[0:1] // current char
+	remStr := str[1:] // remaining string
+
+	perms := getPermutations(remStr) // get perms for remaining string
+
+	allPerms := make([]string, 0) // array to hold all perms of the string based on perms of substring
+
+	// for every perm in the perms of substring
+	for _, perm := range perms {
+	        // add current char at every possible position
+		for i := 0; i <= len(perm); i++ {
+			newPerm := insertAt(i, current, perm)
+			allPerms = append(allPerms, newPerm)
+		}
+	}
+
+	return allPerms
+}
+
+// Insert a char in a word
+func insertAt(i int, char string, perm string) string {
+	start := perm[0:i]
+	end := perm[i:len(perm)]
+	return start + char + end
+}
+
