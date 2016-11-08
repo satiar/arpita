1b5a16f7780c1326e70d0e4d3be1d2f34c9a0dff
Arpita Sati <arpita.sati@hpe.com>
2016-11-08 13:15:33 -0800

    Update linkedlist.go

--- InterviewPrep/linkedlist.go
+++ InterviewPrep/linkedlist.go
@@ -23,3 +23,42 @@ struct node* SortedMerge(struct node* a, struct node* b)
   }
   return(result);
 }
+
+2. Remove duplicates from a linked list O(N)
+Algo 1 : for each element of list, check if it exists in map, if yes, (which means it has occured once, 
+and this current one is a duplicate) remove it (prev.next = curr.next), else add it to the map.
+Algo 2 :(no additional buffer) - for each current element, go delete all subsequent elements with same value
+So keep a curr , and a runner which starts with curr , and goes on till 
+while runner.next !=null {
+  if runner.next.value == curr.value {
+    //delete it
+    runner.next = runner.next.next 
+  } else {
+      runner = runner.next
+  }
+  current = current.next
+  
+3. Return nth from last element of a linked list.
+  Algo: Start 2 pointers, and advance he second by "n". Now when p2 hits the end, p1 is at n distance from end. so p1 is the 
+  nth last element
+// pseudo code
+LinkedListNode nthToLast(LinkedListNode head, int n) { 
+  if (head == null || n < 1) { 
+   return null; 
+  }
+  LinkedListNode p1 = head; 
+  LinkedListNode p2 = head; 
+  for (int j = 0; j < n - 1; ++j) { // skip n-1 steps ahead 
+    if (p2 == null) { 
+      return null; // not found since list size < n 
+    } 
+    p2 = p2.next; 
+  } 
+  while (p2.next != null) { 
+    p1 = p1.next; 
+    p2 = p2.next; 
+  } 
+  return p1; 
+}
+
+  
