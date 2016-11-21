1bfff5268de356a4b52c379ef268f0e64871971c
Arpita Sati <arpita.sati@hpe.com>
2016-11-21 10:06:15 -0800

    Update strings.go

--- InterviewPrep/strings.go
+++ InterviewPrep/strings.go
@@ -471,3 +471,31 @@ func FizzBuzz(amount int) string {
     return results
 } 
 
+27. FizzBuzz with go-routines:
+package main
+
+import "fmt"
+
+func main() {  
+    for out := range FizzBuzz(100) {
+        fmt.Println(out)
+    }
+}
+
+func FizzBuzz(amount int) <-chan string {
+
+    out := make(chan string, amount)
+
+    go func() {
+        for i := 1; i <= amount; i++ {
+            result := ""
+            if i%3 == 0 { result += "Fizz" }
+            if i%5 == 0 { result += "Buzz" }
+            if result == "" { result = fmt.Sprintf("%v", i) }
+            out <- result
+        }
+        close(out)
+    }()
+
+    return out
+}
